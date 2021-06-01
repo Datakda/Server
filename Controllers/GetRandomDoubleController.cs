3a67eea698bbe6c573c99215f7d6ff78c0bcfa95
Datakda <gagan9393@gmail.com>
2021-06-01 22:14:16 +0300

    Добавьте файлы проекта.

---
+++ Controllers/GetRandomDoubleController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Server.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+
+    public class GetRandomDoubleController : ControllerBase
+    {
+        
+        
+        [HttpGet]
+        public string Get(string minMax)
+        {
+            
+            var MinMax =  JsonSerializer.Deserialize<MinMax>(minMax);
+            RandomDouble rnd = new RandomDouble(MinMax);
+           
+
+            return JsonSerializer.Serialize<RandomDouble>(rnd);
+        }
+    }
+}
---
+++ Models/RandomDouble.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Models
+{
+    public class RandomDouble
+    {
+        public double result { get; set; }
+
+
+        public RandomDouble(MinMax minMax) 
+        {
+            Random random = new Random();
+            result = random.NextDouble() * (minMax.Max - minMax.Min) + minMax.Min;
+        }
+
+    }
+}
